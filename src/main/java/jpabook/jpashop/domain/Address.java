df39ad94f9324fa6476027ad6942131520d16328
KimBoSung <ghalsru@naver.com>
2022-04-16 20:11:06 +0900

    엔티티 클래스 개발2 기본생성자, setter는 사용하지 말것

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -11,4 +11,14 @@ public class Address {
     private String city;
     private String street;
     private String zipcode;
+
+    // JPA 특성상 기본 생성자를 만들어 둬 JPA 구현 라이브러리가 객체를 생성할 떄 리플렉션 같은 기술을 사용할 수 있도록하기위함.
+    protected Address() {
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
