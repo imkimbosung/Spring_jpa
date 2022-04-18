5b700a43481b11058d5ab39e760cc9a29f29e9b1
KimBoSung <ghalsru@naver.com>
2022-04-19 00:00:05 +0900

    필드 주입 방법 변경 setter, 필드 보다는 생성자 주입을 권장한다.

--- src/main/java/jpabook/jpashop/service/MemberService.java
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.service;
 
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,10 +11,19 @@ import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class MemberService {
 
-    @Autowired
-    private MemberRepository memberRepository;
+    private final MemberRepository memberRepository;
+
+    /*
+        1. setter, 필드 injection보다 생성자를 권장(테스트코드 작성, 누군가가 수정할 위험성이 사라진다)
+        2. 변수는 final설정 권장
+        > lombok :: @RequiredArgsConstructor final필드만 가지고 자동 생성자 만들어줌
+     */
+//    public MemberService(MemberRepository memberRepository) {
+//        this.memberRepository = memberRepository;
+//    }
 
     //회원 가입
     @Transactional
