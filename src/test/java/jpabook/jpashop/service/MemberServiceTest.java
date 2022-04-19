511b7f8c03fd01b7001df14f744e75893bcb125e
KimBoSung <ghalsru@naver.com>
2022-04-20 00:01:22 +0900

    회원가입 테스트 코드 작성

---
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,46 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class MemberServiceTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    @Rollback(value = false) // 기본적인 rollback이 이뤄지기 때문에 insert 쿼리를 보고싶으면 설정할 것
+    public void 회원가입() throws Exception{
+        // given
+        Member member = new Member();
+        member.setName("kim");
+
+        // when
+        Long saveId = memberService.join(member);
+
+        // then
+        assertEquals(member, memberRepository.findOne(saveId));
+    }
+
+
+    @Test
+    public void 중복_회원_예외() throws Exception{
+        // given
+
+        // when
+
+        // then
+    }
+}
