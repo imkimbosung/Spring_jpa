16e85ea1789fcb2c2de8eb7eb74d0b9298945711
KimBoSung <ghalsru@naver.com>
2022-04-20 00:09:39 +0900

    중복회원 예외 작성 테스트코드

--- src/test/java/jpabook/jpashop/service/MemberServiceTest.java
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -24,7 +24,7 @@ public class MemberServiceTest {
     @Autowired EntityManager em;
 
     @Test
-    //@Rollback(value = false) // 기본적인 rollback이 이뤄지기 때문에 insert 쿼리를 보고싶으면 설정할 성
+    //@Rollback(value = false) // 기본적인 rollback이 이뤄지기 때문에 insert 쿼리를 보고싶으면 설정 (rollback 안됨)
     public void 회원가입() throws Exception{
         // given
         Member member = new Member();
@@ -34,7 +34,7 @@ public class MemberServiceTest {
         Long saveId = memberService.join(member);
 
         // then
-        em.flush(); // DB에 반영(insert 문을 볼 수 있다.)
+        //em.flush(); // DB에 반영(insert 문을 볼 수 있다.) 이건 rollback됨
         assertEquals(member, memberRepository.findOne(saveId));
     }
 
@@ -42,9 +42,22 @@ public class MemberServiceTest {
     @Test
     public void 중복_회원_예외() throws Exception{
         // given
+        Member member1 = new Member();
+        member1.setName("kim1");
+
+        Member member2 = new Member();
+        member2.setName("kim1");
 
         // when
+        memberService.join(member1);
+        try{
+            memberService.join(member2); // 예외 발생
+        } catch (IllegalStateException e){
+            return;
+        }
+
 
         // then
+        fail("예외가 발생해야 한다.");
     }
 }
