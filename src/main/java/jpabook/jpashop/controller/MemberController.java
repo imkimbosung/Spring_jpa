edbc7e568f7162590d4525d94e379570bee6a44e
KimBoSung <ghalsru@naver.com>
2022-05-04 18:27:44 +0900

    회원가입 로직 추가 @NotEmpty, @Valid 어노테이션 의 활용

---
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -0,0 +1,43 @@
+package jpabook.jpashop.controller;
+
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/members/new")
+    public String createForm(Model model){
+        model.addAttribute("memberForm", new MemberForm());
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(@Valid MemberForm from, BindingResult result){ // @Valid을 쓰면 @NotEmpty 등의 validation 기능들을 사용할 수 있다.
+
+        if(result.hasErrors()){
+            return "members/createMemberForm";
+        }
+
+        Address address = new Address(from.getCity(), from.getStreet(), from.getZipcode());
+
+        Member member = new Member();
+        member.setName(from.getName());
+        member.setAddress(address);
+
+        memberService.join(member);
+        return "redirect:/";
+    }
+}
